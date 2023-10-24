995c3f7120b007b0557e45493186decb141efa26
laboratory computer <616318723@qq.com>
2023-10-24 11:13:29 +0800

    init commit

---
+++ DatasetDistillation.py
@@ -0,0 +1,67 @@
+import torch
+import utils
+import pathlib
+import numpy as np
+import os
+import cv2
+
+
+class DatasetDistillation:
+    def __init__(self, batch, obs_spec, act_spec, lr, device):
+        self.batch = batch
+        self.obs_spec = obs_spec
+        self.act_spec = act_spec
+        self.lr = lr
+        self.device = device
+
+        # synthetic batch, initial random
+        obs = np.random.rand(self.batch, *self.obs_spec.shape)
+        action = np.random.rand(self.batch, *self.act_spec.shape)
+        reward = np.random.rand(self.batch, 1)
+        discount = np.random.rand(self.batch, 1)
+        next_obs = np.random.rand(self.batch, *self.obs_spec.shape)
+
+        # TODO: initial fix\orgin
+
+        obs = self.convert(obs, self.obs_spec.minimum, self.obs_spec.maximum)
+        action = self.convert(action, self.act_spec.minimum, self.act_spec.maximum)
+        next_obs = self.convert(next_obs, self.obs_spec.minimum, self.obs_spec.maximum)
+
+        self.obs = torch.tensor(obs, dtype=torch.float, requires_grad=True, device=self.device)
+        self.action = torch.tensor(action, dtype=torch.float, requires_grad=True, device=self.device)
+        self.reward = torch.tensor(reward, dtype=torch.float, requires_grad=True, device=self.device)
+        self.discount = torch.tensor(discount, dtype=torch.float, requires_grad=True, device=self.device)
+        self.next_obs = torch.tensor(next_obs, dtype=torch.float, requires_grad=True, device=self.device)
+
+        self.opt = torch.optim.Adam([self.obs, self.action, self.reward, self.discount, self.next_obs], lr=lr)
+
+    def get_data(self):
+        return self.obs * 255, self.action, self.reward, self.discount, self.next_obs
+
+    def save_img(self):
+        img = np.clip(255 * self.obs.cpu().detach().numpy(), 0, 255).astype(np.uint8)
+        img_n = np.clip(255 * self.next_obs.cpu().detach().numpy(), 0, 255).astype(np.uint8)
+        path = pathlib.Path.cwd().parent.parent / 'sync_obs'
+        pathlib.Path(path).mkdir(parents=True, exist_ok=True)
+        for i in range(img.shape[0]):
+            o = np.transpose(img[i], (1, 2, 0))[:, :, 6:]
+            o_n = np.transpose(img_n[i], (1, 2, 0))[:, :, 6:]
+            cv2.imwrite(os.path.abspath(path) + f'/img_{i}.png', o)
+            cv2.imwrite(os.path.abspath(path) + f'/img_next_{i}.png', o_n)
+
+    def train(self, grad_real, grad_sync):
+        for i in grad_real:
+            i.detach()
+        metrics = {}
+        loss = utils.match_loss(grad_sync, grad_real, self.device, "ours")
+        metrics['DD_loss'] = loss.item()
+        self.opt.zero_grad()
+        # grad = [p.grad for p in [self.obs, self.action, self.reward, self.discount, self.next_obs]]
+        loss.backward()
+        # grad = [p.grad for p in [self.obs, self.action, self.reward, self.discount, self.next_obs]]
+        self.opt.step()
+        return metrics
+
+    def convert(self, data, min, max):
+        data = data * (max - min) + min
+        return data
---
+++ utils.py
@@ -0,0 +1,204 @@
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+import random
+import re
+import time
+
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from omegaconf import OmegaConf
+from torch import distributions as pyd
+from torch.distributions.utils import _standard_normal
+
+
+class eval_mode:
+    def __init__(self, *models):
+        self.models = models
+
+    def __enter__(self):
+        self.prev_states = []
+        for model in self.models:
+            self.prev_states.append(model.training)
+            model.train(False)
+
+    def __exit__(self, *args):
+        for model, state in zip(self.models, self.prev_states):
+            model.train(state)
+        return False
+
+
+def set_seed_everywhere(seed):
+    torch.manual_seed(seed)
+    if torch.cuda.is_available():
+        torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+
+
+def soft_update_params(net, target_net, tau):
+    for param, target_param in zip(net.parameters(), target_net.parameters()):
+        target_param.data.copy_(tau * param.data +
+                                (1 - tau) * target_param.data)
+
+
+def to_torch(xs, device):
+    return tuple(torch.as_tensor(x, device=device).detach() for x in xs)
+
+
+def weight_init(m):
+    if isinstance(m, nn.Linear):
+        nn.init.orthogonal_(m.weight.data)
+        if hasattr(m.bias, 'data'):
+            m.bias.data.fill_(0.0)
+    elif isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
+        gain = nn.init.calculate_gain('relu')
+        nn.init.orthogonal_(m.weight.data, gain)
+        if hasattr(m.bias, 'data'):
+            m.bias.data.fill_(0.0)
+
+
+class Until:
+    def __init__(self, until, action_repeat=1):
+        self._until = until
+        self._action_repeat = action_repeat
+
+    def __call__(self, step):
+        if self._until is None:
+            return True
+        until = self._until // self._action_repeat
+        return step < until
+
+
+class Every:
+    def __init__(self, every, action_repeat=1):
+        self._every = every
+        self._action_repeat = action_repeat
+
+    def __call__(self, step):
+        if self._every is None:
+            return False
+        every = self._every // self._action_repeat
+        if step % every == 0:
+            return True
+        return False
+
+
+class Timer:
+    def __init__(self):
+        self._start_time = time.time()
+        self._last_time = time.time()
+
+    def reset(self):
+        elapsed_time = time.time() - self._last_time
+        self._last_time = time.time()
+        total_time = time.time() - self._start_time
+        return elapsed_time, total_time
+
+    def total_time(self):
+        return time.time() - self._start_time
+
+
+class TruncatedNormal(pyd.Normal):
+    def __init__(self, loc, scale, low=-1.0, high=1.0, eps=1e-6):
+        super().__init__(loc, scale, validate_args=False)
+        self.low = low
+        self.high = high
+        self.eps = eps
+
+    def _clamp(self, x):
+        clamped_x = torch.clamp(x, self.low + self.eps, self.high - self.eps)
+        x = x - x.detach() + clamped_x.detach()
+        return x
+
+    def sample(self, clip=None, sample_shape=torch.Size()):
+        shape = self._extended_shape(sample_shape)
+        eps = _standard_normal(shape,
+                               dtype=self.loc.dtype,
+                               device=self.loc.device)
+        eps *= self.scale
+        if clip is not None:
+            eps = torch.clamp(eps, -clip, clip)
+        x = self.loc + eps
+        return self._clamp(x)
+
+
+def schedule(schdl, step):
+    try:
+        return float(schdl)
+    except ValueError:
+        match = re.match(r'linear\((.+),(.+),(.+)\)', schdl)
+        if match:
+            init, final, duration = [float(g) for g in match.groups()]
+            mix = np.clip(step / duration, 0.0, 1.0)
+            return (1.0 - mix) * init + mix * final
+        match = re.match(r'step_linear\((.+),(.+),(.+),(.+),(.+)\)', schdl)
+        if match:
+            init, final1, duration1, final2, duration2 = [
+                float(g) for g in match.groups()
+            ]
+            if step <= duration1:
+                mix = np.clip(step / duration1, 0.0, 1.0)
+                return (1.0 - mix) * init + mix * final1
+            else:
+                mix = np.clip((step - duration1) / duration2, 0.0, 1.0)
+                return (1.0 - mix) * final1 + mix * final2
+    raise NotImplementedError(schdl)
+
+
+def distance_wb(gwr, gws):
+    shape = gwr.shape
+    if len(shape) == 4: # conv, out*in*h*w
+        gwr = gwr.reshape(shape[0], shape[1] * shape[2] * shape[3])
+        gws = gws.reshape(shape[0], shape[1] * shape[2] * shape[3])
+    elif len(shape) == 3:  # layernorm, C*h*w
+        gwr = gwr.reshape(shape[0], shape[1] * shape[2])
+        gws = gws.reshape(shape[0], shape[1] * shape[2])
+    elif len(shape) == 2: # linear, out*in
+        tmp = 'do nothing'
+    elif len(shape) == 1: # batchnorm/instancenorm, C; groupnorm x, bias
+        gwr = gwr.reshape(1, shape[0])
+        gws = gws.reshape(1, shape[0])
+        return torch.tensor(0, dtype=torch.float, device=gwr.device)
+
+    dis_weight = torch.sum(1 - torch.sum(gwr * gws, dim=-1) / (torch.norm(gwr, dim=-1) * torch.norm(gws, dim=-1) + 0.000001))
+    dis = dis_weight
+    return dis
+
+
+def match_loss(gw_syn, gw_real, device, distant):
+    dis = torch.tensor(0.0).to(device)
+
+    if distant == 'ours':
+        for ig in range(len(gw_real)):
+            gwr = gw_real[ig]
+            gws = gw_syn[ig]
+            dis += distance_wb(gwr, gws)
+
+    elif distant == 'mse':
+        gw_real_vec = []
+        gw_syn_vec = []
+        for ig in range(len(gw_real)):
+            gw_real_vec.append(gw_real[ig].reshape((-1)))
+            gw_syn_vec.append(gw_syn[ig].reshape((-1)))
+        gw_real_vec = torch.cat(gw_real_vec, dim=0)
+        gw_syn_vec = torch.cat(gw_syn_vec, dim=0)
+        dis = torch.sum((gw_syn_vec - gw_real_vec)**2)
+
+    elif distant == 'cos':
+        gw_real_vec = []
+        gw_syn_vec = []
+        for ig in range(len(gw_real)):
+            gw_real_vec.append(gw_real[ig].reshape((-1)))
+            gw_syn_vec.append(gw_syn[ig].reshape((-1)))
+        gw_real_vec = torch.cat(gw_real_vec, dim=0)
+        gw_syn_vec = torch.cat(gw_syn_vec, dim=0)
+        dis = 1 - torch.sum(gw_real_vec * gw_syn_vec, dim=-1) / (torch.norm(gw_real_vec, dim=-1) * torch.norm(gw_syn_vec, dim=-1) + 0.000001)
+
+    else:
+        exit('unknown distance function: %s'%distant)
+
+    return dis
