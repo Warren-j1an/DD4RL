3244f1548ff41d9938fc96733882a6c3b7aad00c
Warren-j1an <wj1an0512@gmail.com>
2023-11-01 16:36:37 +0800

    add log to img difference

--- DatasetDistillation.py
+++ DatasetDistillation.py
@@ -24,23 +24,24 @@ class DatasetDistillation:
         for _ in range(nstep):
             discount_ *= discount
 
-        obs = np.random.rand(1000, *self.obs_spec.shape)
-        action = np.random.rand(1000, *self.act_spec.shape)
-        reward = np.linspace(0, 6, 1001)[:-1].reshape(-1, 1)  # action_repeat * nstep = 6
-        discount = np.full(shape=(1000, 1), fill_value=discount_)
-        next_obs = np.random.rand(1000, *self.obs_spec.shape)
+        self.init_obs = np.random.rand(1000, *self.obs_spec.shape)
+        self.init_action = np.random.rand(1000, *self.act_spec.shape)
+        self.init_reward = np.linspace(0, 6, 1001)[:-1].reshape(-1, 1)  # action_repeat * nstep = 6
+        self.init_discount = np.full(shape=(1000, 1), fill_value=discount_)
+        self.init_next_obs = np.random.rand(1000, *self.obs_spec.shape)
 
-        self.obs = torch.tensor(obs, dtype=torch.float, requires_grad=True, device=self.device)
-        self.action = torch.tensor(action, dtype=torch.float, requires_grad=True, device=self.device)
-        self.reward = torch.tensor(reward, dtype=torch.float, requires_grad=False, device=self.device)
-        self.discount = torch.tensor(discount, dtype=torch.float, requires_grad=False, device=self.device)
-        self.next_obs = torch.tensor(next_obs, dtype=torch.float, requires_grad=True, device=self.device)
+        self.obs = torch.tensor(self.init_obs, dtype=torch.float, requires_grad=True, device=self.device)
+        self.action = torch.tensor(self.init_action, dtype=torch.float, requires_grad=True, device=self.device)
+        self.reward = torch.tensor(self.init_reward, dtype=torch.float, requires_grad=False, device=self.device)
+        self.discount = torch.tensor(self.init_discount, dtype=torch.float, requires_grad=False, device=self.device)
+        self.next_obs = torch.tensor(self.init_next_obs, dtype=torch.float, requires_grad=True, device=self.device)
+
+        self.init_obs = self.obs.cpu().detach().numpy()
 
         self.opt_obs = torch.optim.Adam([self.obs, self.next_obs], lr=self.lr_obs)
         self.opt_action = torch.optim.Adam([self.action], lr=self.lr_action)
         self.index = None
         self.statistic = collections.defaultdict(int)
-        self.grad = collections.defaultdict(int)
 
     def get_data(self, batch):
         reward_ = batch[2].numpy()
@@ -69,9 +70,6 @@ class DatasetDistillation:
         self.opt_action.zero_grad(set_to_none=True)
         loss.backward()
         grad = [p.grad for p in [self.obs, self.action, self.reward, self.discount, self.next_obs]]
-        for i in range(1000):
-            self.grad[i] += (torch.sum(grad[0][i]).cpu().numpy() /
-                             (grad[0][i].shape[0] * grad[0][i].shape[1] * grad[0][i].shape[2]))
         self.opt_obs.step()
         self.opt_action.step()
 
@@ -81,7 +79,8 @@ class DatasetDistillation:
         return metrics
 
     def save_img(self, global_step):
-        img = np.clip(self.obs.cpu().detach().numpy() * 255, 0, 255).astype(np.uint8)
+        obs_log = self.obs.cpu().detach().numpy()
+        img = np.clip(obs_log * 255, 0, 255).astype(np.uint8)
         reward = self.reward.cpu().detach().numpy()
         img_n = np.clip(self.next_obs.cpu().detach().numpy() * 255, 0, 255).astype(np.uint8)
         path = pathlib.Path.cwd() / f'sync_obs/{global_step}'
@@ -93,19 +92,20 @@ class DatasetDistillation:
             o_n = np.transpose(img_n[i], (1, 2, 0))[:, :, 6:]
             cv2.imwrite(os.path.abspath(path) + f'/img_{i}.png', o)
             cv2.imwrite(os.path.abspath(path) + f'/img_next_{i}.png', o_n)
-            obs = Image(os.path.abspath(path) + f'/img_{i}.png')
-            obs_next = Image(os.path.abspath(path) + f'/img_next_{i}.png')
-            ws.add_image(obs, f'A{i + 1}')
+            # obs = Image(os.path.abspath(path) + f'/img_{i}.png')
+            # obs_next = Image(os.path.abspath(path) + f'/img_next_{i}.png')
+            # ws.add_image(obs, f'A{i + 1}')
             ws[f'B{i + 1}'] = reward[i][0]
-            ws.add_image(obs_next, f'C{i + 1}')
+            # ws.add_image(obs_next, f'C{i + 1}')
             ws[f'D{i + 1}'] = self.statistic[f'reward_{i}']
-            ws[f'E{i + 1}'] = self.grad[i] / global_step if global_step != 0 else self.grad[i]
+            ws[f'E{i + 1}'] = np.sum(obs_log[i] - self.init_obs[i]) * 255
+            ws[f'F{i + 1}'] = np.sum((obs_log[i] - self.init_obs[i]) != 0)
         wb.save(os.path.abspath(path) + f'/sync_data_{global_step}.xlsx')
-        for i in range(1000):
-            obs = pathlib.Path(os.path.abspath(path) + f'/img_{i}.png')
-            obs_next = pathlib.Path(os.path.abspath(path) + f'/img_next_{i}.png')
-            obs.unlink()
-            obs_next.unlink()
+        # for i in range(1000):
+        #     obs = pathlib.Path(os.path.abspath(path) + f'/img_{i}.png')
+        #     obs_next = pathlib.Path(os.path.abspath(path) + f'/img_next_{i}.png')
+        #     obs.unlink()
+        #     obs_next.unlink()
 
     def convert(self, obs, action, reward, discount, next_obs):
         obs_ = F.normalize(obs, p=2, dim=1)
